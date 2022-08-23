f6e93f50053812227fbde7fdc6ad83f611029f40
jiang <1711940547@qq.com>
2022-08-23 22:15:49 +0800

    提交项目

---
+++ FirstPro/FirstPro/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading;
+
+namespace FirstPro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Thread t1 = new Thread(_ => {
+
+                Console.WriteLine("hello you will win");
+            });
+            Console.Read();
+        }
+    }
+}
